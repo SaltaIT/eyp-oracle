c948a81f3e467d41cd49ba22578eea17f7caede4
Jordi Prats <jprats@systemadmin.es>
2016-11-10 14:33:41 +0100

    afegit variable ensure

--- manifests/compat/package.pp
+++ manifests/compat/package.pp
@@ -3,6 +3,7 @@ define oracle::compat::package(
                                 $packaganame = $name,
                                 $baseurl     = 'https://oss.oracle.com/projects/compat-oracle/dist/files/Enterprise_Linux',
                                 $arch        = 'i686',
+                                $ensure      = 'present',
                               ) {
   Exec {
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
@@ -10,8 +11,16 @@ define oracle::compat::package(
 
   include ::oracle
 
-  exec { "oracle compat package ${packaganame} ${version} ${arch}":
-    command => "rpm -Uvh ${baseurl}/${packaganame}-${version}.${oracle::params::package_versiontag}.${arch}.rpm",
-    unless  => "rpm -q  ${packaganame}",
+  if($ensure=='present')
+  {
+    exec { "oracle compat package ${packaganame} ${version} ${arch}":
+      command => "rpm -Uvh ${baseurl}/${packaganame}-${version}.${oracle::params::package_versiontag}.${arch}.rpm",
+      unless  => "rpm -q  ${packaganame}",
+    }
+  }
+  else
+  {
+    #absent
+    fail('currently unsupported')
   }
 }
