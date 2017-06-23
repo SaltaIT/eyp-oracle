e9d3aacc34f969f438c09cf97519bc90de572dd5
Jordi Prats <jprats@systemadmin.es>
2017-06-23 14:12:19 +0200

    lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,12 @@
 #
 class oracle::install inherits oracle {
 
-  if($oracle::manage_package)
-  {
-    # package here, for example: 
-    #package { $oracle::params::package_name:
-    #  ensure => $oracle::package_ensure,
-    #}
-  }
+  # if($oracle::manage_package)
+  # {
+  #   # package here, for example:
+  #   #package { $oracle::params::package_name:
+  #   #  ensure => $oracle::package_ensure,
+  #   #}
+  # }
 
 }
